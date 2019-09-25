9d9600ae991a2c65b4ac65bddd1fd1fa8e4b3d54
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-09-25 19:44:40 +0500

    --Commit: add image to Lvl1 folder added

--- src/redux/actions/FolderActions.js
+++ src/redux/actions/FolderActions.js
@@ -1,4 +1,4 @@
-import { ADD_FOLDER_TO_ROOT, ADD_IMAGE_TO_ROOT, SET_FOLDER_NAME } from "../types"
+import { ADD_FOLDER_TO_ROOT, ADD_IMAGE_TO_ROOT, SET_FOLDER_NAME, ADD_IMAGE_TO_FOLDER } from "../types"
 
 export const setFolderName = (name) => {
     return {
@@ -17,6 +17,13 @@ export const addFolder = (data) => {
 export const addImageToRoot = (data) => {
     return {
         type: ADD_IMAGE_TO_ROOT,
+        payload: data
+    }
+}
+
+export const addImageToFolder = (data) => {
+    return {
+        type: ADD_IMAGE_TO_FOLDER,
         payload: data
     }
 }
