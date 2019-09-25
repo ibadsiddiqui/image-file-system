13d3a577fbfface4586f3ba3c7b583008d6c0e1a
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-09-25 15:23:40 +0500

    --Commit: upload image from camera and gallery added

--- src/screens/LinksScreen.js
+++ src/screens/LinksScreen.js
@@ -1,13 +1,48 @@
+import * as ImagePicker from 'expo-image-picker';
 import React from 'react';
-import { ScrollView, StyleSheet, TextInput, Dimensions, View, Button } from 'react-native';
+import { Alert, Button, Dimensions, StyleSheet, TextInput, View } from 'react-native';
 import { connect } from 'react-redux';
-import { mapStateToProps, mapDispatchToProps } from '../redux/dispatchers';
-// import { ExpoLinksView } from '@expo/samples';
+import uuidV4 from 'uuid/v4';
+import { mapDispatchToProps, mapStateToProps } from '../redux/dispatchers';
 
 class LinksScreen extends React.Component {
   addRootFolder = () => {
-    this.props.addFolder({ name: "/root", type: "folder", data: [] })
+    const { folderName } = this.props
+    this.props.addFolder({ name: folderName, path: '/' + folderName, type: "folder", data: [], id: uuidV4() })
   }
+
+  FromGallery = async () => {
+    const response = await ImagePicker.launchImageLibraryAsync({ allowsEditing: true, mediaTypes: ImagePicker.MediaTypeOptions.Images })
+    let imageName = response.uri.slice(response.uri.lastIndexOf("/") + 1)
+    this.props.addImageToRoot({ id: uuidV4(), name: imageName, path: '/' + imageName, diskUri: response.uri, type: "image", })
+  }
+
+
+  FromCamera = async () => {
+    const response = await ImagePicker.launchCameraAsync({ allowsEditing: true, mediaTypes: ImagePicker.MediaTypeOptions.Images })
+    let imageName = response.uri.slice(response.uri.lastIndexOf("/") + 1)
+    this.props.addImageToRoot({ id: uuidV4(), name: imageName, path: '/' + imageName, diskUri: response.uri, type: "image", })
+  }
+
+  addImageToRoot = () => {
+    Alert.alert(
+      'Alert Title',
+      'My Alert Msg',
+      [
+        {
+          text: 'Open Gallery',
+          onPress: this.FromGallery
+        },
+        {
+          text: 'Open Camera',
+          onPress: this.FromCamera,
+          style: 'cancel',
+        },
+      ],
+      { cancelable: false },
+    );
+  }
+
   render() {
     return (
       <View style={{ flex: 1, alignSelf: 'center', justifyContent: 'center', alignItems: "center" }}>
@@ -18,6 +53,9 @@ class LinksScreen extends React.Component {
         </View>
         <View style={{ alignSelf: 'center' }}>
           <Button onPress={this.addRootFolder} title="Add Folder" />
+        </View>
+        <View style={{ alignSelf: 'center' }}>
+          <Button onPress={this.addImageToRoot} title="Add Image" />
         </View>
       </View>
     );
