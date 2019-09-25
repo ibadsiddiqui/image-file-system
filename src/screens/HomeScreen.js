58c29c37e85b48d8d011af61a4f129b24016ec3a
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-09-25 15:23:16 +0500

    --Commit: image rendering and folder added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,54 @@
+import { Feather } from '@expo/vector-icons';
 import React from 'react';
-import { StyleSheet, View, } from 'react-native';
-import { Feather } from '@expo/vector-icons'
-import { connect } from 'react-redux'
-import { mapStateToProps, mapDispatchToProps } from '../redux/dispatchers';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { mapDispatchToProps, mapStateToProps } from '../redux/dispatchers';
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container} >
-        <Feather name="folder" size={42} />
+      <View style={styles.container}>
+        <FlatList
+          data={this.props.foldersList.data}
+          keyExtractor={(item, idx) => idx}
+          renderItem={({ item }) => {
+            if (item.type === "folder")
+              return (
+                <View style={{
+                  height: 75,
+                  flexDirection: 'row',
+                  alignItems: 'flex-start',
+                  borderWidth: 1,
+                  borderColor: "transparent",
+                  borderBottomColor: 'black',
+                  paddingHorizontal: 50,
+                  paddingVertical: 5,
+                  marginVertical: 0
+                }}>
+                  <Feather name="folder" size={20} />
+                  <Text style={{ marginHorizontal: 25 }}>{item.name}</Text>
+                </View>
+              )
+            else return (
+              <View style={{
+                height: 75,
+                flexDirection: 'row',
+                alignItems: 'flex-start',
+                borderWidth: 1,
+                borderColor: "transparent",
+                borderBottomColor: 'black',
+                paddingHorizontal: 50,
+                paddingVertical: 5,
+                marginVertical: 0
+              }}>
+                <View style={{ height: 50, width: 50 }}>
+                  <Image source={{ uri: item.diskUri }} style={{ height: 60, width: 35 }} />
+                </View>
+                <Text style={{ marginHorizontal: 25 }}>{item.name}</Text>
+              </View>
+            )
+          }
+          }
+        />
       </View>
     );
   }
@@ -22,6 +63,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingVertical: 100,
   },
 });
 
