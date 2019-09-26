c0dc4c1ed6719566f5aa4645796faec23239c1fa
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-09-26 12:10:56 +0500

    --Commit: view lvl1 folder details screen added

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,14 +1,15 @@
-import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-
-import MainTabNavigator from './MainTabNavigator';
 import ViewFolderData from '../screens/ViewFolderData';
+import ViewFolderInsideLvl1Screen from '../screens/ViewFolderInsideLvl1';
+import MainTabNavigator from './MainTabNavigator';
+
 
 export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
     Main: MainTabNavigator,
-    ViewFolder: ViewFolderData
+    ViewFolder: ViewFolderData,
+    ViewFolderInsideLvl1: ViewFolderInsideLvl1Screen,
   })
 );
